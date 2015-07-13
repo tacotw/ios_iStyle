218b7bee550e5921afa079664d3a51956e288cd7
Taco Chang <tacotw@positionhis-lm.tpcity.corp.yahoo.com>
2015-07-13 18:02:11 +0800

    Initial Commit

---
+++ iStyle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iStyle
+//
+//  Created by Taco Chang on 2015/7/13.
+//  Copyright (c) 2015年 Taco Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iStyle/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iStyle
+//
+//  Created by Taco Chang on 2015/7/13.
+//  Copyright (c) 2015年 Taco Chang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
